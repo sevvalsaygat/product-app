f79f45359984543c78a6f18320df9b67ea7a8a66
sevvalsaygat <sevvallsstt@gmail.com>
2023-04-07 16:45:17 +0300

    feat: add axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,26 @@
+import { Product } from '@components';
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex justify-center align-center">
+      <div className="w-1/2 flex flex-col">
+        <button className="p-3 border border-gray-800 bg-gray-500 rounded-sm mb-52 ">
+          New Product
+        </button>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Stock quantity</th>
+              <th>Photograph</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Product />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
---
+++ pages/products/new.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <form>
+      <input placeholder="product" />
+      <input placeholder="Price"></input>
+      <input placeholder="Stock quantity"></input>
+      <input placeholder="Photograph"></input>
+      <button type="submit"></button>
+    </form>
+  );
+}
